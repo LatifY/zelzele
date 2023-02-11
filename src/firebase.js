c9dd83f25ba8ed88d341eb18683b762ca8de05b3
Latif Yılmaz <latifyilmaza@gmail.com>
2023-02-11 08:28:44 +0300

    ..

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process?.env?.REACT_APP_API_KEY,
+  apiKey: "AIzaSyCPi_pND7R7s_q36olkBNP802Qlx39y7a4",
   authDomain: "zelzele-a5426.firebaseapp.com",
   databaseURL: "https://zelzele-a5426-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "zelzele-a5426",
   storageBucket: "zelzele-a5426.appspot.com",
   messagingSenderId: "670728612513",
-  appId: process?.env?.REACT_APP_ID,
+  appId: "1:670728612513:web:654a0ef332a2d6c4279ee0",
   measurementId: "G-4HMYYR2ZFJ"
 };
 
